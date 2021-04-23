f47e417d5b774a22244c4583bd964d1e693fea9b
Junior Medina <juniiormediina@gmail.com>
2021-04-23 17:59:25 -0500

    se comenta funcion de vitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,13 @@ import '../global.css';
 
 import CartProvider from '@store/Cart';
 
-export function reportWebVitals(metric) {
-  // analytics - Calibre
-  // esto nos ayuda a hacerle seguimientos exitos a nuestros sitios web
+// export function reportWebVitals(metric) {
+//   // analytics - Calibre
+//   // esto nos ayuda a hacerle seguimientos exitos a nuestros sitios web
 
-  console.log(metric)
-  //serverAnalytics.log(metric)
-}
+//   console.log(metric)
+//   //serverAnalytics.log(metric)
+// }
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props
